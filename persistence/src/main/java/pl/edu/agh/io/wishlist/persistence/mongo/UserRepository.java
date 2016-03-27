98137774768a6e5814a66e355df58785a26fda91
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-03-27 14:29:17 +0200

    First of MongoRepository interfaces to get data from Mongo

---
+++ persistence/src/main/java/pl/edu/agh/io/wishlist/persistence/mongo/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.io.wishlist.persistence.mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import pl.edu.agh.io.wishlist.domain.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByLogin(String login);
+
+}
