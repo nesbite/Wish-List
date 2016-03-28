ae828ecc2f54f648817e79f30738b266c65e1b6d
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-03-28 12:41:29 +0200

    User with annotated id

--- domain/src/main/java/pl/edu/agh/io/wishlist/domain/User.java
+++ domain/src/main/java/pl/edu/agh/io/wishlist/domain/User.java
@@ -1,16 +1,19 @@
 package pl.edu.agh.io.wishlist.domain;
 
+import org.springframework.data.annotation.Id;
+
 public class User {
+
     @Id
     private long id;
     private String login;
     private String password;
     private List<Long> friends;
 
-    public User(){}
+    public User() {
+    }
 
-    public User(long id, String login, String password) {
-        this.id = id;
+    public User(String login, String password) {
         this.login = login;
         this.password = password;
         this.friends = new ArrayList<>();
