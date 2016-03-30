87c213d3b318ad13f847e1cacb61d84f4f10a74c
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-03-30 19:28:01 +0200

    Working template of integration testing

--- server/src/main/java/pl/edu/agh/io/wishlist/server/Application.java
+++ server/src/main/java/pl/edu/agh/io/wishlist/server/Application.java
@@ -1,48 +1,17 @@
 package pl.edu.agh.io.wishlist.server;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
-import pl.edu.agh.io.wishlist.domain.User;
-import pl.edu.agh.io.wishlist.persistence.UserRepository;
 
 @SpringBootApplication
 @ImportResource("classpath:pl/edu/agh/io/wishlist/server/beans.xml")
 @ComponentScan(basePackages = "pl.edu.agh.io.wishlist")
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    private UserRepository repository;
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-//		repository.deleteAll();
-
-        // save a couple of customers
-//        repository.save(new User("Alice", "Smith"));
-//        repository.save(new User("Bob", "Smith"));
-
-        // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        for (User user : repository.findAll()) {
-            System.out.println(user);
-        }
-        System.out.println();
-
-        // fetch an individual customer
-        System.out.println("Customer found with findByFirstName('Alice'):");
-        System.out.println("--------------------------------");
-        System.out.println(repository.findByLogin("Alice"));
-
     }
 
 }
---
+++ server/src/test/java/pl/edu/agh/io/wishlist/server/ApplicationTest.java
@@ -0,0 +1,56 @@
+package pl.edu.agh.io.wishlist.server;
+
+import com.jayway.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import pl.edu.agh.io.wishlist.domain.User;
+import pl.edu.agh.io.wishlist.persistence.UserRepository;
+
+import static com.jayway.restassured.RestAssured.when;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class ApplicationTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Before
+    public void setUp() throws Exception {
+        RestAssured.port = port;
+
+        User user = new User("jan", "pass_jan");
+
+        repository.deleteAll();
+        repository.save(user);
+    }
+
+    @Test
+    public void testUsersGet() throws Exception {
+        when().
+                get("/users/jan").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                body("login", Matchers.is("jan")).
+                body("password", Matchers.is("pass_jan"));
+    }
+
+    // TODO write more tests here or create another, similar test-class
+    // see: http://www.jayway.com/2014/07/04/integration-testing-a-spring-boot-application/
+
+}
