5bb0d4c9ea3a13a3425a850cde03943bfef03ae3
Nesbite <grzegorz.siatka@gmail.com>
2016-05-01 18:48:58 +0200

    Validators added

---
+++ domain/src/main/java/pl/edu/agh/io/wishlist/domain/validation/EmailExistsException.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.io.wishlist.domain.validation;
+
+@SuppressWarnings("serial")
+public class EmailExistsException extends Throwable {
+
+    public EmailExistsException(final String message) {
+        super(message);
+    }
+
+}
